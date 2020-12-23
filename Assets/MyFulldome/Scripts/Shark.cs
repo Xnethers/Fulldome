8ce90599a05c211203a079d5d9028c2a521981de
z44440000z <z44440000z@gmail.com>
2020-12-23 11:51:17 +0800

    1.場景2完成 2.動畫完成

---
+++ Assets/MyFulldome/Scripts/Shark.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun.Demo.SlotRacer.Utils;
+
+public class Shark : MonoBehaviour
+{
+    public Animator animator;
+    public string animateName;
+    public bool isTrue;
+    public SplineWalker bezier;
+    public float speed = 1;
+
+    void OnEnable() 
+    {
+        animator.SetBool(animateName,isTrue);
+        bezier.Speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
